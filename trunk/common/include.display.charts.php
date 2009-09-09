c3a67961b70681e7db88fb59e26d54d8c60e602d
jjhatf02 <jjhatf02@4f638a4c-9826-11de-aa05-a1f2ad4bc971>
2009-09-09 14:20:20 +0000

    Added several interesting charts to the charts page.

--- trunk/common/include.display.charts.php
+++ trunk/common/include.display.charts.php
@@ -1,11 +1,70 @@
 <?php
 
+function report_chart_member_distribution(){
+	$query  = "SELECT 'Directors' name, 1 pid, COUNT(*) number ";
+	$query .= "FROM members WHERE position < 10 ";
+	$query .= "UNION ";
+	$query .= "SELECT p.name, p.id, COUNT(*) number ";
+	$query .= "FROM members m ";
+	$query .= "JOIN position p ON m.position = p.id ";
+	$query .= "WHERE position != 20 AND position > 9 ";
+	$query .= "GROUP BY p.name, p.id ";
+	$query .= "ORDER BY pid;";
+	$result = mysql_query($query);
+	$val = array();
+	while($row = mysql_fetch_assoc($result)){
+		$val[] = $row;
+	}
+	//Generate the URL
+	$url = "http://chart.apis.google.com/chart?cht=p&chco=FF0000&chtt=Member+Distribution&chd=t:";
+	for($i = 0; $i < sizeof($val); $i++){
+		$url .= $val[$i]['number'];
+		if( ($i + 1) < sizeof($val) ){
+			$url  .= ",";
+		}
+	}
+	$url .="&chs=500x200&chl=";
+	for($i = 0; $i < sizeof($val); $i++){
+		$url .= str_replace("&", "and", str_replace(" ", "+", $val[$i]['name']));
+		if( ($i + 1) < sizeof($val) ){
+			$url  .= "|";
+		}
+	}
+	return $url;
+}
 
+function report_chart_current_member_standing(){
+	$query  = "SELECT count(*) number, sum(warning), sum(trouble) ";
+	$query .= "FROM v_standing_g vsg ";
+	$query .= "JOIN members m ON vsg.member = m.id ";
+	$query .= "WHERE vsg.semester = (SELECT `id` FROM semester s WHERE s.startday < NOW() ORDER BY s.startday DESC LIMIT 1) ";
+	$query .= "	AND vsg.meeting_type = 1;";
+	$result = mysql_query($query);
+	$row = mysql_fetch_row($result);
+	$standing['good'] =  $row[0] - $row[1] - $row[2];
+	$standing['warning'] =  $row[1];
+	$standing['bad'] =  $row[2];
+	
+	//Generate the URL
+	$url  = "http://chart.apis.google.com/chart?cht=p&chco=FF0000&chtt=Member+Standing+For+Current+Semester&chd=t:";
+	$url .= $standing['good'] . "," . $standing['warning'] . "," . $standing['bad'];
+	$url .= "&chs=500x200&chl=";
+	$url .= "Good|Warning|Bad";
+	return $url;
+}
+
+
+function report_chart_major_member_count($position){
+	$query = "SELECT COUNT(*) number FROM members WHERE `position` = " . $position . ";";
+	$result = mysql_query($query);
+	$row = mysql_fetch_row($result);
+	return $row[0];
+}
 function report_chart_member_breakdown($position){
 	$query = "SELECT `name` FROM `position` WHERE `id` = " . $position . ";";
 	$result = mysql_query($query);
 	$row = mysql_fetch_row($result);
-	$positionname =  str_replace(" ", "+", $row[0]);;
+	$positionname =  str_replace(" ", "+", $row[0]);
 	
 	$query  = "SELECT j.shortname name, count(*) number ";
 	$query .= "FROM members m ";
@@ -69,6 +128,7 @@ function report_chart_committee_pie_chart(){
 	}
 	return $url;
 }
+
 
 function report_chart_major_pie_chart(){
 	$query = "SELECT j.shortname name, COUNT(*) as number ";
