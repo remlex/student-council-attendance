41f9ef6b6fd456679efcf4decc68156497823530
jjhatf02 <jjhatf02@4f638a4c-9826-11de-aa05-a1f2ad4bc971>
2009-09-09 14:21:21 +0000

    Changed the default database name.  Not really an important change, but it reflects the development environment.

--- configs/db.php
+++ configs/db.php
@@ -4,6 +4,6 @@
 // CONNECT TO THE DATABASE
 //----------------------------------------------------------------------------------------------------------------------------------//
 $link = mysql_connect('localhost', 'root', '') or die('Could not connect to mysql server.' );
-mysql_select_db('attendance', $link) or die('Could not select database.');
+mysql_select_db('sssc_attendance', $link) or die('Could not select database.');
 
 ?>
